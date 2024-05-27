b975ef10083c69bff16a4a3f1e507ef241d130ca
Ibrahim Raimi <raimiibrahim44@gmail.com>
2024-05-27 10:00:23 +0100

    chore(db): database

--- src/routes/auth/email-verification/+page.svelte
+++ src/routes/auth/email-verification/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-	import type { PageData } from './$types.js';
+	import type { PageData } from "./$types.js";
 	// import EmailVerificationForm from '$lib/components/Forms/EmailVerificationForm.svelte';
 	// import FormHeader from '$lib/components/Forms/FormHeader.svelte';
 	// import LuciaFormContainer from '$lib/components/Forms/LuciaFormContainer.svelte';
 
-	export let data: PageData;
+	// export let data: PageData;
 </script>
 
-<div class="flex items-center justify-center w-screen h-svh bg-background">
+<div class="flex h-svh w-screen items-center justify-center bg-background">
 	<!-- <EmailVerificationForm data={data.form} showBadge showContainer /> -->
 
 	<!-- <LuciaFormContainer showBadge>
